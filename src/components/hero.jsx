bacf43106ffaaa4377151c0f12f9a9863323bd05
David Josipović <davidjosipovic19@gmail.com>
2023-05-16 15:37:41 +0200

    !

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <div className='HeroText'>
             <h1>Hello there,</h1>
             <h1>My name is <span className='purple'>David</span>,</h1>
-            <p>and this is my portfolio.</p>
+            <p>and this is my portfolio!</p>
             <p>I am a CS student based in Croatia who has a strong interest in designing web pages.</p>
             
         </div>
