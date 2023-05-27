db818e34b053365bb4dad7acf9b1292435be3bea
David Josipović <davidjosipovic19@gmail.com>
2023-05-27 11:19:32 +0200

    aaa

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import './App.css';
 import Project1 from './pages/Project1';
 import Project2 from './pages/Project2';
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <BrowserRouter>
+    
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project1" element={<Project1 />} />
         <Route path="/project2" element={<Project2 />} />
     </Routes>
-  </BrowserRouter>
   );
 }
 
