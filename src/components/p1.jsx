40cf561bb496f288a6f1488a48856cdcafb47d1d
David Josipović <davidjosipovic19@gmail.com>
2023-06-04 17:59:08 +0200

    txt

--- src/components/p1.jsx
+++ src/components/p1.jsx
@@ -17,7 +17,7 @@ function P1() {
       <h3>Projekt 1 <a className='github' href="https://github.com/davidjosipovic/apartment-website" target='_blank' rel="noopener noreferrer"><img src="./github.svg" alt="github" /></a></h3>
       <p>Web stranica za apartmane</p>
       <p className='txt'>
-        Moj prvi projekt je web stranica za bookiranje apartmana koja prikazuje dostupne apartmane i omogućava korisnicima jednostavno rezerviranje.<br/> Kroz atraktivan dizajn i intuitivno korisničko iskustvo, cilj stranice je olakšati pronalazak i rezervaciju željenog smještaja.
+        Moj prvi projekt je web stranica za iznajmljivanje apartmana koja prikazuje dostupne apartmane i omogućava korisnicima jednostavno rezerviranje.<br/> Kroz atraktivan dizajn i intuitivno korisničko iskustvo, cilj stranice je olakšati pronalazak i rezervaciju željenog smještaja.
       </p>
       <div className='slideshow-container'>
         <div className={`mySlides ${slideIndex === 0 ? 'active' : ''}`}>
