a123db634ebe8741ddde07b04c25aa6eedd12acd
davidjosipovic <davidjosipovic19@gmail.com>
2023-11-04 15:57:07 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Project1 from './pages/Project1';
-import Project2 from './pages/Project2';
 import Home from './pages/Home';
 import { Routes, Route } from "react-router-dom";
 
@@ -9,8 +7,7 @@ function App() {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/project1" element={<Project1 />} />
-        <Route path="/project2" element={<Project2 />} />
+        
     </Routes>
   );
 }
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,19 +1,19 @@
 import './projects.css';
-import { Link,Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function Projects() {
   return (
     <div  className="Projects">
         <div className='Project'>
             <h3>Project 1</h3>
             <p>Apartments website</p>
-            <Link to="/project1"><img id='project1' src="./Apartman.png" alt="Apartman" /></Link>
+            <Link to="/"><img id='project1' src="./Apartman.png" alt="Apartman" /></Link>
         </div>
         <div  className='Project'>
             <h3>Project 2</h3>
             <p>Construction firm website</p>
-            <Link to="/project2"><img id='project2' src="./Mir.png" alt="Mir" /></Link>
+            <Link to="/"><img id='project2' src="./Mir.png" alt="Mir" /></Link>
         </div>
-     <Outlet />  
+   
     </div>
   );
 }
